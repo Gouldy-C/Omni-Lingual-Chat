634d9a8d846413a82836f557fa019d7096e6d345
Christian Gouldy <123622913+Gouldy-C@users.noreply.github.com>
2023-12-27 12:54:26 -0800

    Added header to pricing

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -4,6 +4,7 @@ import PricingCard from "@/components/PricingCard";
 export default function PricingPage() {
   return (
     <div className="m-5">
+      <h1>Pricing</h1>
       <PricingCard redirect={true} />
     </div>
   )
