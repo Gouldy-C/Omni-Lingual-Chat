205f49ba1ae5369c342a13725c7685531ecf43bc
Christian Gouldy <123622913+Gouldy-C@users.noreply.github.com>
2024-03-07 08:46:16 -0800

    Update page.tsx

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -4,7 +4,6 @@ import PricingCard from "@/components/PricingCard";
 export default function PricingPage() {
   return (
     <div className="m-5">
-      <h1>Pricing</h1>
       <PricingCard redirect={true} />
     </div>
   )
