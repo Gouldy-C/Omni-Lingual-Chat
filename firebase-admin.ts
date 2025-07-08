575bb7b2d4afdb061b103944f76403ef7611a1bf
Gouldy-C <mail4clg@gmail.com>
2025-07-08 12:56:38 -0600

    fixed issue with button interaction

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -1,26 +1,24 @@
-import { initFirestore } from "@auth/firebase-adapter"
-import admin from "firebase-admin"
+import { initFirestore } from "@auth/firebase-adapter";
+import admin from "firebase-admin";
 
-let app
+let app;
 
 const credentials = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
+  project_id: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   privateKey: process.env.FIREBASE_PRIVATE_KEY,
 };
 
-
 if (!admin.apps.length) {
   app = admin.initializeApp({
-    credential: admin.credential.cert(credentials)
+    credential: admin.credential.cert(credentials),
   });
 }
 
+const adminDb = initFirestore({
+  credential: admin.credential.cert(credentials),
+});
 
-const adminDb = initFirestore( {
-  credential: admin.credential.cert(credentials)
-})
+const adminAuth = admin.auth(app);
 
-const adminAuth = admin.auth(app)
-
-export { adminDb, adminAuth }
+export { adminDb, adminAuth };
